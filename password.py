9a7d8434d8c600e93ac77f19fb22170d2680afe3
tuna912 <76613109+tuna912@users.noreply.github.com>
2024-12-04 20:37:39 +0300

    Create password.py

---
+++ password.py
@@ -0,0 +1,8 @@
+import random
+uzunluk = int(input(şifre uzunluğu giriniz))
+sifre = ""
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+print(random.choice(uzunluk)
+for i in range(uzunluk):
+    sifre+=random.choice(sembol)
+print("şifreniz", şifre)
